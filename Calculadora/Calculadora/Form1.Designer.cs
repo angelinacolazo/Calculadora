4faf75f53355358618c20ac6a2a9d2b521f24219
hector pollo <pollo2419@hotmail.com>
2019-09-01 09:47:36 -0500

    bien

---
+++ Calculadora/Calculadora/Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace Calculadora
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.barra = new System.Windows.Forms.TextBox();
+            this.b7 = new System.Windows.Forms.Button();
+            this.b8 = new System.Windows.Forms.Button();
+            this.b9 = new System.Windows.Forms.Button();
+            this.b4 = new System.Windows.Forms.Button();
+            this.b5 = new System.Windows.Forms.Button();
+            this.b6 = new System.Windows.Forms.Button();
+            this.b1 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.bp = new System.Windows.Forms.Button();
+            this.b0 = new System.Windows.Forms.Button();
+            this.bc = new System.Windows.Forms.Button();
+            this.bce = new System.Windows.Forms.Button();
+            this.vm = new System.Windows.Forms.Button();
+            this.bd = new System.Windows.Forms.Button();
+            this.bpo = new System.Windows.Forms.Button();
+            this.bs = new System.Windows.Forms.Button();
+            this.br = new System.Windows.Forms.Button();
+            this.bi = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // barra
+            // 
+            this.barra.Location = new System.Drawing.Point(43, 12);
+            this.barra.Name = "barra";
+            this.barra.Size = new System.Drawing.Size(315, 20);
+            this.barra.TabIndex = 0;
+            // 
+            // b7
+            // 
+            this.b7.Location = new System.Drawing.Point(12, 62);
+            this.b7.Name = "b7";
+            this.b7.Size = new System.Drawing.Size(55, 35);
+            this.b7.TabIndex = 1;
+            this.b7.Text = "7";
+            this.b7.UseVisualStyleBackColor = true;
+            this.b7.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // b8
+            // 
+            this.b8.Location = new System.Drawing.Point(73, 62);
+            this.b8.Name = "b8";
+            this.b8.Size = new System.Drawing.Size(55, 35);
+            this.b8.TabIndex = 2;
+            this.b8.Text = "8";
+            this.b8.UseVisualStyleBackColor = true;
+            this.b8.Click += new System.EventHandler(this.B8_Click);
+            // 
+            // b9
+            // 
+            this.b9.Location = new System.Drawing.Point(134, 62);
+            this.b9.Name = "b9";
+            this.b9.Size = new System.Drawing.Size(55, 35);
+            this.b9.TabIndex = 3;
+            this.b9.Text = "9";
+            this.b9.UseVisualStyleBackColor = true;
+            this.b9.Click += new System.EventHandler(this.B9_Click);
+            // 
+            // b4
+            // 
+            this.b4.Location = new System.Drawing.Point(12, 103);
+            this.b4.Name = "b4";
+            this.b4.Size = new System.Drawing.Size(55, 35);
+            this.b4.TabIndex = 4;
+            this.b4.Text = "4";
+            this.b4.UseVisualStyleBackColor = true;
+            this.b4.Click += new System.EventHandler(this.B4_Click);
+            // 
+            // b5
+            // 
+            this.b5.Location = new System.Drawing.Point(73, 103);
+            this.b5.Name = "b5";
+            this.b5.Size = new System.Drawing.Size(55, 35);
+            this.b5.TabIndex = 5;
+            this.b5.Text = "5";
+            this.b5.UseVisualStyleBackColor = true;
+            this.b5.Click += new System.EventHandler(this.B5_Click);
+            // 
+            // b6
+            // 
+            this.b6.Location = new System.Drawing.Point(134, 103);
+            this.b6.Name = "b6";
+            this.b6.Size = new System.Drawing.Size(55, 35);
+            this.b6.TabIndex = 6;
+            this.b6.Text = "6";
+            this.b6.UseVisualStyleBackColor = true;
+            this.b6.Click += new System.EventHandler(this.B6_Click);
+            // 
+            // b1
+            // 
+            this.b1.Location = new System.Drawing.Point(12, 144);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(55, 35);
+            this.b1.TabIndex = 7;
+            this.b1.Text = "1";
+            this.b1.UseVisualStyleBackColor = true;
+            this.b1.Click += new System.EventHandler(this.B1_Click);
+            // 
+            // b2
+            // 
+            this.b2.Location = new System.Drawing.Point(73, 144);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(55, 35);
+            this.b2.TabIndex = 8;
+            this.b2.Text = "2";
+            this.b2.UseVisualStyleBackColor = true;
+            this.b2.Click += new System.EventHandler(this.B2_Click);
+            // 
+            // b3
+            // 
+            this.b3.Location = new System.Drawing.Point(134, 144);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(55, 35);
+            this.b3.TabIndex = 9;
+            this.b3.Text = "3";
+            this.b3.UseVisualStyleBackColor = true;
+            this.b3.Click += new System.EventHandler(this.B3_Click);
+            // 
+            // bp
+            // 
+            this.bp.Location = new System.Drawing.Point(12, 185);
+            this.bp.Name = "bp";
+            this.bp.Size = new System.Drawing.Size(55, 35);
+            this.bp.TabIndex = 10;
+            this.bp.Text = ".";
+            this.bp.UseVisualStyleBackColor = true;
+            this.bp.Click += new System.EventHandler(this.Bp_Click);
+            // 
+            // b0
+            // 
+            this.b0.Location = new System.Drawing.Point(73, 185);
+            this.b0.Name = "b0";
+            this.b0.Size = new System.Drawing.Size(116, 35);
+            this.b0.TabIndex = 11;
+            this.b0.Text = "0";
+            this.b0.UseVisualStyleBackColor = true;
+            this.b0.Click += new System.EventHandler(this.B0_Click);
+            // 
+            // bc
+            // 
+            this.bc.Location = new System.Drawing.Point(215, 62);
+            this.bc.Name = "bc";
+            this.bc.Size = new System.Drawing.Size(55, 35);
+            this.bc.TabIndex = 12;
+            this.bc.Text = "C";
+            this.bc.UseVisualStyleBackColor = true;
+            this.bc.Click += new System.EventHandler(this.Bc_Click);
+            // 
+            // bce
+            // 
+            this.bce.Location = new System.Drawing.Point(277, 62);
+            this.bce.Name = "bce";
+            this.bce.Size = new System.Drawing.Size(115, 35);
+            this.bce.TabIndex = 13;
+            this.bce.Text = "CE";
+            this.bce.UseVisualStyleBackColor = true;
+            this.bce.Click += new System.EventHandler(this.Bce_Click);
+            // 
+            // vm
+            // 
+            this.vm.Location = new System.Drawing.Point(215, 103);
+            this.vm.Name = "vm";
+            this.vm.Size = new System.Drawing.Size(55, 35);
+            this.vm.TabIndex = 14;
+            this.vm.Text = "*";
+            this.vm.UseVisualStyleBackColor = true;
+            this.vm.Click += new System.EventHandler(this.Button14_Click);
+            // 
+            // bd
+            // 
+            this.bd.Location = new System.Drawing.Point(276, 103);
+            this.bd.Name = "bd";
+            this.bd.Size = new System.Drawing.Size(55, 35);
+            this.bd.TabIndex = 15;
+            this.bd.Text = "/";
+            this.bd.UseVisualStyleBackColor = true;
+            this.bd.Click += new System.EventHandler(this.Bd_Click);
+            // 
+            // bpo
+            // 
+            this.bpo.Location = new System.Drawing.Point(337, 103);
+            this.bpo.Name = "bpo";
+            this.bpo.Size = new System.Drawing.Size(55, 35);
+            this.bpo.TabIndex = 16;
+            this.bpo.Text = "%";
+            this.bpo.UseVisualStyleBackColor = true;
+            this.bpo.Click += new System.EventHandler(this.Button16_Click);
+            // 
+            // bs
+            // 
+            this.bs.Location = new System.Drawing.Point(215, 144);
+            this.bs.Name = "bs";
+            this.bs.Size = new System.Drawing.Size(55, 35);
+            this.bs.TabIndex = 17;
+            this.bs.Text = "+";
+            this.bs.UseVisualStyleBackColor = true;
+            this.bs.Click += new System.EventHandler(this.Bs_Click);
+            // 
+            // br
+            // 
+            this.br.Location = new System.Drawing.Point(215, 185);
+            this.br.Name = "br";
+            this.br.Size = new System.Drawing.Size(55, 35);
+            this.br.TabIndex = 18;
+            this.br.Text = "-";
+            this.br.UseVisualStyleBackColor = true;
+            this.br.Click += new System.EventHandler(this.Br_Click);
+            // 
+            // bi
+            // 
+            this.bi.Location = new System.Drawing.Point(276, 144);
+            this.bi.Name = "bi";
+            this.bi.Size = new System.Drawing.Size(116, 76);
+            this.bi.TabIndex = 19;
+            this.bi.Text = "=";
+            this.bi.UseVisualStyleBackColor = true;
+            this.bi.Click += new System.EventHandler(this.Bi_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 225);
+            this.Controls.Add(this.bi);
+            this.Controls.Add(this.br);
+            this.Controls.Add(this.bs);
+            this.Controls.Add(this.bpo);
+            this.Controls.Add(this.bd);
+            this.Controls.Add(this.vm);
+            this.Controls.Add(this.bce);
+            this.Controls.Add(this.bc);
+            this.Controls.Add(this.b0);
+            this.Controls.Add(this.bp);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.b6);
+            this.Controls.Add(this.b5);
+            this.Controls.Add(this.b4);
+            this.Controls.Add(this.b9);
+            this.Controls.Add(this.b8);
+            this.Controls.Add(this.b7);
+            this.Controls.Add(this.barra);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox barra;
+        private System.Windows.Forms.Button b7;
+        private System.Windows.Forms.Button b8;
+        private System.Windows.Forms.Button b9;
+        private System.Windows.Forms.Button b4;
+        private System.Windows.Forms.Button b5;
+        private System.Windows.Forms.Button b6;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button bp;
+        private System.Windows.Forms.Button b0;
+        private System.Windows.Forms.Button bc;
+        private System.Windows.Forms.Button bce;
+        private System.Windows.Forms.Button vm;
+        private System.Windows.Forms.Button bd;
+        private System.Windows.Forms.Button bpo;
+        private System.Windows.Forms.Button bs;
+        private System.Windows.Forms.Button br;
+        private System.Windows.Forms.Button bi;
+    }
+}
+
---
+++ Calculadora/Calculadora/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora
+{
+    public partial class Form1 : Form
+    {
+        double n1, n2, re;
+        string ope;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Button14_Click(object sender, EventArgs e)
+        {
+            ope = "*";
+            n1 = double.Parse(barra.Text);
+            barra.Clear();
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            barra.Text = barra.Text + "7";
+        }
+        private void Button16_Click(object sender, EventArgs e)
+        {
+            ope = "%";
+            n1 = double.Parse(barra.Text);
+            barra.Clear();
+        }
+        private void B8_Click(object sender, EventArgs e)
+        {
+            barra.Text = barra.Text + "8";
+        }
+        private void B9_Click(object sender, EventArgs e)
+        {
+            barra.Text = barra.Text + "9";
+        }
+        private void B4_Click(object sender, EventArgs e)
+        {
+            barra.Text = barra.Text + "4";
+        }
+        private void B5_Click(object sender, EventArgs e)
+        {
+            barra.Text = barra.Text + "5";
+        }
+        private void B6_Click(object sender, EventArgs e)
+        {
+            barra.Text = barra.Text + "6";
+        }
+        private void B1_Click(object sender, EventArgs e)
+        {
+            barra.Text = barra.Text + "1";
+        }
+        private void B2_Click(object sender, EventArgs e)
+        {
+            barra.Text = barra.Text + "2";
+        }
+        private void B3_Click(object sender, EventArgs e)
+        {
+            barra.Text = barra.Text + "3";
+        }
+        private void Bp_Click(object sender, EventArgs e)
+        {
+            barra.Text = barra.Text + ".";
+        }
+        private void Bs_Click(object sender, EventArgs e)
+        {
+            ope = "+";
+            n1 = double.Parse(barra.Text);
+            barra.Clear();
+        }
+        private void Br_Click(object sender, EventArgs e)
+        {
+            ope = "-";
+            n1 = double.Parse(barra.Text);
+            barra.Clear();
+        }
+
+        private void Bd_Click(object sender, EventArgs e)
+        {
+            ope = "/";
+            n1 = double.Parse(barra.Text);
+            barra.Clear();
+        }
+
+        private void Bi_Click(object sender, EventArgs e)
+        {
+            n2 = double.Parse(barra.Text);
+            if (ope=="+")
+            {
+                re = n1 + n2;
+                barra.Text = re.ToString();
+            }
+            else if (ope=="-")
+            {
+                re = n1 - n2;
+                barra.Text = re.ToString();
+            }
+            else if (ope == "*")
+            {
+                re = n1 * n2;
+                barra.Text = re.ToString();
+            }
+            else if (ope == "/")
+            {
+                re = n1 / n2;
+                barra.Text = re.ToString();
+            }
+            else if (ope == "%")
+            {
+                re = n1 /100 * n2;
+                barra.Text = re.ToString();
+            }
+        }
+        private void Bce_Click(object sender, EventArgs e)
+        {
+            if (barra.Text.Length == 1)
+            {
+                barra.Text = "";
+            }
+            else
+            {
+                barra.Text = barra.Text.Substring(0, barra.Text.Length - 1);
+            }
+        }
+        private void B0_Click(object sender, EventArgs e)
+        {
+            barra.Text = barra.Text + "0";
+        }
+
+        private void Bc_Click(object sender, EventArgs e)
+        {
+            barra.Clear();
+        }
+    }
+}
